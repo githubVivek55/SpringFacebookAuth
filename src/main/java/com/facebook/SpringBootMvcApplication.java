11ad884c0ac8caf776fedb2129e30682a22cdc34
githubVivek55 <vivekpce55@outlook.com>
2017-11-30 22:30:38 +0530

    Remove Spring security and Integrate Angular

--- src/main/java/com/facebook/SpringBootMvcApplication.java
+++ src/main/java/com/facebook/SpringBootMvcApplication.java
@@ -2,21 +2,12 @@ package com.facebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan("com.facebook")
-@EnableOAuth2Sso
-public class SpringBootMvcApplication extends WebSecurityConfigurerAdapter {
+public class SpringBootMvcApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcApplication.class, args);
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
 	}
 }
